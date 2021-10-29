1f72501def4ad8f88ed4d5c2697941ec9ecce8d8
Deniz Orsel <denizonerorsel@gmail.com>
2021-10-29 11:15:28 +0200

    Initial commit

---
+++ src/pages/Favorites.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useContext } from 'react'
+import FavoritesContext from '../store/favorites-context'
+import MeetupList from '../components/meetups/MeetupList'
+
+const FavoritesPage = () => {
+    const favoritesCtx =  useContext(FavoritesContext);
+
+    let content;
+
+    if (favoritesCtx.totalFavorites === 0) {
+        content = <p>No favs</p>
+    } else {
+        content = <MeetupList meetups={favoritesCtx.favorites} />
+    }
+
+    return (
+        <section>
+            <h1>My favorites</h1>
+            {content}
+        </section>
+    )
+}
+
+export default FavoritesPage
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+import { useHistory } from 'react-router-dom';
+
+const NewMeetupPage = () => {
+
+
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'PUT_FIREBASE_DB_ENDPOINT_HERE',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+            ).then(() => {
+                history.replace('/')
+            });
+    }
+
+    return (
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </section>
+    )
+}
+
+export default NewMeetupPage
